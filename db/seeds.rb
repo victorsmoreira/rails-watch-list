04122c3d0438e875830f254242c4fb03e7f16f22
Victor Moreira <victorsmoreira@outlook.com>
2021-08-19 14:58:35 -0300

    routes and controllers done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Movie.title
+  overview = Faker::Movies::HarryPotter.quote
+  poster_url = 'https://source.unsplash.com/650x974/?movie'
+  rating = rand(4.0..10.0).round(1)
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
