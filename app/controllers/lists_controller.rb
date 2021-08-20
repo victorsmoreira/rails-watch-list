7bdefd3b44e453c1b37663eb30bb99cdbd21a1de
Victor Moreira <victorsmoreira@outlook.com>
2021-08-20 16:06:20 -0300

    protection around imgs. destroy list implemented. minor css patch

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %i[show edit update]
+  before_action :set_list, only: %i[show edit update destroy]
 
   def index
     @lists = List.all
@@ -26,6 +26,11 @@ class ListsController < ApplicationController
   def update
     @list.update(list_params)
     redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to root_path
   end
 
   private
