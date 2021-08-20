6e4a828114d4798cea5ca76623e0aa92f798a145
Victor Moreira <victorsmoreira@outlook.com>
2021-08-20 13:22:42 -0300

    root route config. logo inclusion

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[index show new create] do
+  root to: 'lists#index'
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
